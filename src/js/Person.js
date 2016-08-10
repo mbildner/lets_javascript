90bf0d706ff3dc910f1af21a9afebe507bd0e04d
mbildner@pivotal.io <pivotal@astoria.ny.pivotallabs.com>
2016-08-10 11:53:56 -0400

    remove lodash from app code

--- src/js/Person.js
+++ src/js/Person.js
@@ -1,11 +1,9 @@
-import _ from 'lodash'
-
 export class Person {
   constructor(name){
     this.name = name
   }
 
   mapName(letterTransformer){
-    return _.first(_.map(this.name.split(), letterTransformer))
+    return this.name.split().map(letterTransformer).join('')
   }
 }
